c91deedcbba5b3a5d135c12d32cd95e1dbb992eb
Oluwafemijohn1 <femiogundipe01@gmail.com>
2024-04-05 03:22:19 +0100

    Food2Fork Project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Food2Fork"
+include(":androidApp")
+include(":shared")
